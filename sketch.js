2af913c1396d82d521ef4f1808c2b295e07eff57
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2021-07-27 15:59:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var grid = 50;
+var width = 1366;
+var carGroup1,logGroup1;
+var grassHeight = 100;
+var carAnimation1,carAnimation2, logAnimation, playerAnimation,playerImage;
+var school;
+var city,cityAnimation;
+function preload()
+{carAnimation1=loadAnimation("images/car1.png");
+carAnimation2=loadAnimation("images/car2.png");
+playerAnimation=loadAnimation("images/Player-03.png");
+logAnimation=loadAnimation("images/log2.png");
+ cityAnimation=loadAnimation("images/city1.png");
+}
+
+function setup() {
+  createCanvas(1366,700);
+  carGroup1 = new Group();
+  logGroup1 = new Group();
+  city=createSprite(width/2,-1500);
+  city.addAnimation("city",cityAnimation);
+  for (var i=0;i<6;i++){
+    var bottomGrass1 = createSprite(683,height-50-(i*400),width,grassHeight);
+    
+    if(i%2===0){
+      var road= createSprite(683,height-150-(i*400)-grassHeight,width,300,);
+      road.shapeColor="black";
+    }
+    bottomGrass1.shapeColor="grey";
+  }
+  for(var i = 0; i<40; i++){
+    cars = new Car(2);
+    carGroup1.add(cars.spt);
+  }
+ for(var i = 0; i<40; i++){
+   log=new Log(-2);
+   logGroup1.add(log.spt);
+ }
+ }
+ 
+function draw() {
+  background("skyblue");
+  for(i=1;i<logGroup1.length;i++){
+ if(logGroup1[i].x<0)
+   logGroup1[i].x=width;
+ }
+ for(i=1;i<carGroup1.length;i++){
+   if(carGroup1[i].x<0)
+   {
+   carGroup1[i].x=width;
+ }
+if(carGroup1[i].x>width){
+  carGroup1[i].x=0;
+}
+ }
+
+  drawSprites();
+}
+
