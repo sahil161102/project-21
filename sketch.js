e85ba00d7718e5215b18645a9ca92ad8011c8e65
sahil161102 <85942526+sahil161102@users.noreply.github.com>
2021-07-27 16:09:14 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,11 +4,11 @@ var carGroup1,logGroup1;
 var grassHeight = 100;
 var carAnimation1,carAnimation2, logAnimation, playerAnimation,playerImage;
 function preload()
-{carAnimation1=loadAnimation("images/car1.png");
-carAnimation2=loadAnimation("images/car2.png");
-playerAnimation=loadAnimation("images/Player-03.png");
-logAnimation=loadAnimation("images/log2.png");
- cityAnimation=loadAnimation("images/city1.png");
+{carAnimation1=loadAnimation("car1.png");
+carAnimation2=loadAnimation("car2.png");
+playerAnimation=loadAnimation("Player-03.png");
+logAnimation=loadAnimation("log2.png");
+ cityAnimation=loadAnimation("city1.png");
 }
 
 function setup() {
